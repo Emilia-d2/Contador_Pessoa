0fac86fd94d2593034bb2a42b06b8566b00d44b5
Emília Ferlin <emilia.ferlin@sbsistemas.com.br>
2022-06-29 17:46:49 -0300

    ajustes no aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -10,92 +10,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contagem = 0;
 
   // ignore: non_constant_identifier_names
   void SaiPessoa() {
-    print('Foi');
+    setState(() {
+      contagem--;
+    });
   }
 
   // ignore: non_constant_identifier_names
   void EntraPessoa() {
-    print('tchau');
+    setState(() {
+      contagem++;
+    });
   }
+
+  bool get isEmpty => contagem == 0;
+  bool get isFull => contagem == 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'Pode entrar!',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.w800,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/imagens/imagem.jpg'),
+            fit: BoxFit.cover,
           ),
         ),
-        //const SizedBox(height: 30), é uma maneira de dar espaçamento
-        const Padding(
-          padding: EdgeInsets.only(top: 35, left: 10, right: 10, bottom: 50),
-          child: Text(
-            '0',
-            style: TextStyle(
-              fontSize: 120,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-              onPressed: SaiPessoa,
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.white,
-                fixedSize: const Size(90, 90),
-                primary: Colors.grey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
+            Text(
+              isFull ? 'Lotado!' : 'Pode entrar!',
+              style: TextStyle(
+                fontSize: 30,
+                color: isFull ? Colors.red : Colors.white,
+                fontWeight: FontWeight.w800,
               ),
-              child: const Text(
-                'Saiu',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
+            ),
+            //const SizedBox(height: 30), é uma maneira de dar espaçamento
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 35, left: 10, right: 10, bottom: 50),
+              child: Text(
+                contagem.toString(),
+                style: const TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
                 ),
               ),
             ),
-            const SizedBox(width: 30),
-            TextButton(
-              onPressed: EntraPessoa,
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.white,
-                fixedSize: const Size(90, 90),
-                primary: Colors.grey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : SaiPessoa,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(90, 90),
+                    primary: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
-                //padding: const EdgeInsets.all(30),
-              ),
-              child: const Text(
-                'Entrou',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
+                const SizedBox(width: 30),
+                TextButton(
+                  onPressed: isFull ? null : EntraPessoa,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(90, 90),
+                    primary: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    //padding: const EdgeInsets.all(30),
+                  ),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
-      ]),
+      ),
     );
   }
 }
